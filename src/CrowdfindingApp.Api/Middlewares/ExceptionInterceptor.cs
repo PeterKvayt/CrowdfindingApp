fe14b64e68af33ee6b3ce81a600e22f95bf550c9
PeterKvayt <peter.kvayt@gmail.com>
2021-01-07 21:26:26 +0300

    Delete unnecessary using.

--- src/CrowdfindingApp.Api/Middlewares/ExceptionInterceptor.cs
+++ src/CrowdfindingApp.Api/Middlewares/ExceptionInterceptor.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using CrowdfindingApp.Common.DataTransfers.Errors;
 using CrowdfindingApp.Common.Extensions;
