196ac6e403ffe4db845dc27ba7232a768db8203e
PeterKvayt <peter.kvayt@gmail.com>
2021-01-14 23:19:25 +0300

    Remove unnecessary field.

--- src/CrowdfindingApp.Common/Messages/User/UpdateUserRequestMessage.cs
+++ src/CrowdfindingApp.Common/Messages/User/UpdateUserRequestMessage.cs
@@ -3,7 +3,6 @@ namespace CrowdfindingApp.Common.Messages.User
 {
     public class UpdateUserRequestMessage : MessageBase
     {
-        public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Name { get; set; }
