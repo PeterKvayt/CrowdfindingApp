7fc9da2b5ca38fd3ec359e537145d5f7f44d52db
PeterKvayt <peter.kvayt@gmail.com>
2021-01-24 17:01:35 +0300

    Increase token lifetime.

--- src/CrowdfindingApp.Common/Immutable/AuthenticationOptions.cs
+++ src/CrowdfindingApp.Common/Immutable/AuthenticationOptions.cs
@@ -12,7 +12,7 @@ namespace CrowdfindingApp.Common.Immutable
         public const string ValidIssuer = "CrowdfindingApp";
         public const string ValidAudience = "CrowdfindingAppClient";
         const string Key = "b9FIqypA)6fTN~nw1:G!u&?`i<3Nrs760{)r:iAh%KJ`w`<hiP$f4E>{-zlC1SU";
-        public const int LifeTime = 1;
+        public const int LifeTime = 300;
 
         public static SymmetricSecurityKey IssuerSigningKey => new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Key));
     }
