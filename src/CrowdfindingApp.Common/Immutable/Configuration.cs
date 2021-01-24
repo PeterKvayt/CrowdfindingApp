f00a428b44534b300d932b9d5d8d22bedec57555
PeterKvayt <peter.kvayt@gmail.com>
2021-01-24 21:13:58 +0300

    Update configuration consts.

--- src/CrowdfindingApp.Common/Immutable/Configuration.cs
+++ src/CrowdfindingApp.Common/Immutable/Configuration.cs
@@ -4,5 +4,15 @@ namespace CrowdfindingApp.Common.Immutable
     public static class Configuration
     {
         public const string Connection = "DefaultConnection";
+        public const string ClientHost = nameof(ClientHost);
+    }
+
+    public static class EmailConfig
+    {
+        public const string Section = nameof(EmailConfig);
+        public const string Host = nameof(Host);
+        public const string Port = nameof(Port);
+        public const string Mail = nameof(Mail);
+        public const string Password = nameof(Password);
     }
 }
