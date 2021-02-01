1c834f3901d163d7adb3ea1492107160bf65c66b
PeterKvayt <peter.kvayt@gmail.com>
2021-02-01 21:57:52 +0300

    Update profile component.

--- src/CrowdfindingApp.Client/src/app/pages/profile/profile.component.ts
+++ src/CrowdfindingApp.Client/src/app/pages/profile/profile.component.ts
@@ -3,6 +3,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Base } from '../Base';
 import { UserService } from 'src/app/services/user.service';
 import { Title } from '@angular/platform-browser';
+import { ReplyMessage } from 'src/app/models/replies/ReplyMessage';
+import { UserInfo } from 'src/app/models/replies/user/UserInfo';
+import { AuthenticationService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -14,13 +17,34 @@ export class ProfileComponent extends Base implements OnInit {
   constructor(
     public router: Router,
     public activatedRoute: ActivatedRoute,
-    public accountService: UserService,
-    private titleService: Title
+    public userService: UserService,
+    private titleService: Title,
+    private authService: AuthenticationService
   ) {
     super(router, activatedRoute);
   }
 
+  public userInfo: UserInfo;
+
   public ngOnInit(): void {
     this.titleService.setTitle('Профиль');
+    this.setUserInfo();
+  }
+
+  private setUserInfo(): void {
+    this.subscriptions.add(
+      this.userService.getUserInfo().subscribe(
+        (reply: ReplyMessage<UserInfo>) => {
+          this.userInfo = reply.value;
+          console.log(this.userInfo);
+        },
+        error => { this.handleError(error); }
+      )
+    );
+  }
+
+  public onSignOutClick(): void {
+    this.authService.signOut();
+    this.redirect('sign-in');
   }
 }
