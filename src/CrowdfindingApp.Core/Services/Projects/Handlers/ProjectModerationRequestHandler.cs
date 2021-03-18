ba13018d73b9b94c999017c2224ffa6458a90173
PeterKvayt <peter.kvayt@gmail.com>
2021-03-18 20:58:13 +0300

    Unnecessary code removing.

--- src/CrowdfindingApp.Core/Services/Projects/Handlers/ProjectModerationRequestHandler.cs
+++ src/CrowdfindingApp.Core/Services/Projects/Handlers/ProjectModerationRequestHandler.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Threading.Tasks;
-using CrowdfindingApp.Common.DataTransfers.Projects;
-using CrowdfindingApp.Common.Handlers;
 using CrowdfindingApp.Common.Messages;
 using CrowdfindingApp.Common.Messages.Projects;
 using CrowdfindingApp.Core.Services.Projects.Validators;
@@ -12,31 +10,18 @@ using CrowdfindingApp.Data.Common.BusinessModels;
 using CrowdfindingApp.Common.Enums;
 using CrowdfindingApp.Data.Common.Filters;
 using System.Collections.Generic;
-using System.Linq;
-using CrowdfindingApp.Common.DataTransfers.Rewards;
-using CrowdfindingApp.Common.DataTransfers.Questions;
 
 namespace CrowdfindingApp.Core.Services.Projects.Handlers
 {
     public class ProjectModerationRequestHandler : SaveProjectRequestHandlerBase<ProjectModerationRequestMessage, ReplyMessageBase>
     {
-        //private IMapper _mapper;
-        //private IProjectRepository _projectRepository;
-        //private IRewardRepository _rewardRepository;
-        //private IRewardGeographyRepository _rewardGeographyRepository;
-        //private IQuestionRepository _questionRepository;
-
         public ProjectModerationRequestHandler(IMapper mapper, 
             IProjectRepository projectRepository, 
             IRewardRepository rewardRepository, 
             IRewardGeographyRepository rewardGeographyRepository,
             IQuestionRepository questionRepository) : base(mapper, projectRepository, rewardRepository, rewardGeographyRepository, questionRepository)
         {
-            //_mapper = mapper ?? throw new NullReferenceException(nameof(mapper));
-            //_projectRepository = projectRepository ?? throw new NullReferenceException(nameof(projectRepository));
-            //_rewardRepository = rewardRepository ?? throw new NullReferenceException(nameof(rewardRepository));
-            //_rewardGeographyRepository = rewardGeographyRepository ?? throw new NullReferenceException(nameof(rewardGeographyRepository));
-            //_questionRepository = questionRepository ?? throw new NullReferenceException(nameof(questionRepository));
+
         }
 
         protected override async Task<ReplyMessageBase> ValidateRequestMessageAsync(ProjectModerationRequestMessage requestMessage)
@@ -68,80 +53,11 @@ namespace CrowdfindingApp.Core.Services.Projects.Handlers
             return await ProcessAsync(request);
         }
 
-        //private async Task<Guid> SaveProjectAsync(ProjectInfo projectInfo)
-        //{
-        //    var isNew = projectInfo.Id.IsNullOrWhiteSpace();
-        //    var project = _mapper.Map<Project>(projectInfo);
-        //    SetDefaultValues(project, isNew);
-
-        //    var projectId = project.Id;
-        //    if(isNew)
-        //    {
-        //        projectId = await _projectRepository.AddAsync(project);
-        //    }
-        //    else
-        //    {
-        //        await _projectRepository.UpdateAsync(project);
-        //    }
-        //    return projectId;
-        //}
-
         protected override void SetDefaultValues(Project project, bool isNew)
         {
             base.SetDefaultValues(project, isNew);
             project.Status = (int)ProjectStatus.Moderation;
         }
-
-        //private async Task SaveQuestionsAsync(ProjectInfo projectInfo, Guid projectId)
-        //{
-        //    var questions = projectInfo.Questions.Select(x => MapToQuestion(x, projectId));
-        //    foreach(var question in questions)
-        //    {
-        //        if(question.Id == Guid.Empty)
-        //        {
-        //            await _questionRepository.AddAsync(question);
-        //        }
-        //        else
-        //        {
-        //            await _questionRepository.UpdateAsync(question);
-        //        }
-        //    }
-        //}
-
-        //private Question MapToQuestion(QuestionInfo info, Guid projectId)
-        //{
-        //    var question = _mapper.Map<Question>(info);
-        //    question.ProjectId = projectId;
-        //    return question;
-        //}
-
-        //private async Task SaveRewardsAsync(ProjectInfo projectInfo, Guid projectId)
-        //{
-        //    foreach(var rewardinfo in projectInfo.Rewards)
-        //    {
-        //        var reward = MapToReward(rewardinfo, projectId);
-        //        var rewardId = reward.Id;
-        //        if(reward.Id == Guid.Empty)
-        //        {
-        //            rewardId = await _rewardRepository.AddAsync(reward);
-        //        }
-        //        else
-        //        {
-        //            await _rewardRepository.UpdateAsync(reward);
-        //        }
-
-        //        var deliveryCountries = rewardinfo.DeliveryCountries.Select(x => new RewardGeography(rewardId, new Guid(x.Key), x.Value.Value)).ToList();
-        //        await _rewardGeographyRepository.SubstituteRangeAsync(deliveryCountries, rewardId);
-        //    }
-        //}
-
-        //private Reward MapToReward(RewardInfo info, Guid projectId)
-        //{
-        //    var reward = _mapper.Map<Reward>(info);
-        //    reward.ProjectId = projectId;
-        //    return reward;
-        //}
-
-
+     
     }
 }
--- src/CrowdfindingApp.Core/Services/Projects/Handlers/SaveDraftProjectRequestHandler.cs
+++ src/CrowdfindingApp.Core/Services/Projects/Handlers/SaveDraftProjectRequestHandler.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 using AutoMapper;
 using CrowdfindingApp.Common.Enums;
 using CrowdfindingApp.Common.Extensions;
-using CrowdfindingApp.Common.Handlers;
 using CrowdfindingApp.Common.Immutable;
 using CrowdfindingApp.Common.Messages;
 using CrowdfindingApp.Common.Messages.Projects;
@@ -14,17 +13,13 @@ namespace CrowdfindingApp.Core.Services.Projects.Handlers
 {
     public class SaveDraftProjectRequestHandler : SaveProjectRequestHandlerBase<SaveDraftProjectRequestMessage, ReplyMessageBase>
     {
-        //private readonly IMapper _mapper;
-        //private readonly IProjectRepository _repository;
-
         public SaveDraftProjectRequestHandler(IMapper mapper,
             IProjectRepository projectRepository,
             IRewardRepository rewardRepository,
             IRewardGeographyRepository rewardGeographyRepository,
             IQuestionRepository questionRepository) : base(mapper, projectRepository, rewardRepository, rewardGeographyRepository, questionRepository)
         {
-            //_mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-            //_repository = repository ?? throw new ArgumentNullException(nameof(repository));
+
         }
 
         protected override async Task<ReplyMessageBase> ValidateRequestMessageAsync(SaveDraftProjectRequestMessage requestMessage)
