693de8d5276e3ac9cd59858d94692a41a1feb717
PeterKvayt <peter.kvayt@gmail.com>
2021-04-07 00:06:47 +0300

    Password validator added.

---
+++ src/CrowdfindingApp.Core/Services/Users/Validators/PasswordValidator.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Threading.Tasks;
+using CrowdfindingApp.Common.Extensions;
+using FluentValidation;
+
+namespace CrowdfindingApp.Core.Services.Users.Validators
+{
+    public class PasswordValidator : AbstractValidator<string>
+    {
+        public PasswordValidator()
+        {
+            RuleFor(x => x).NotEmpty().WithErrorCode(UserErrorKeys.EmptyPassword);
+
+            RuleFor(x => x).Must(x => x.Length < MinLength)
+                .When(x => !x.IsNullOrWhiteSpace())
+                .WithErrorCode(UserErrorKeys.InvalidPasswordLength)
+                .WithCustomMessageParameters(x => Task.FromResult(new[] { MinLength.ToString() }));
+        }
+
+        public bool Confirm(string password, string confirmPassword)
+        {
+            return password == confirmPassword;
+        }
+
+        private const int MinLength = 2;
+
+    }
+}
