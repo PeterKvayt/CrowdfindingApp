d1b7c7ccfecf51dca9b3a7b5061636487116964b
PeterKvayt <peter.kvayt@gmail.com>
2021-04-25 17:35:05 +0300

    Content added.

--- src/CrowdfindingApp.Client/src/app/pages/help-page/help-page.component.ts
+++ src/CrowdfindingApp.Client/src/app/pages/help-page/help-page.component.ts
@@ -18,26 +18,37 @@ export class HelpPageComponent extends Base implements OnInit {
     private titleService: Title
   ) { super(router, activatedRoute); }
 
-    public platformName = 'Crowdfinding app';
-    public maxValue = '4 947';
-    public tax = '13%';
-    public profitPercent = '10%';
+  public platformName = 'Crowdfinding app';
+  public maxValue = '4 947';
+  public tax = '13%';
+  public profitPercent = '10%';
 
-    public helpRoute = Routes.help;
+  public helpRoute = Routes.help;
 
   public tabs: GenericLookupItem<boolean, string>[] = [
-    {key: true, value: 'Как работает платформа'}, // 0
-    {key: false, value: 'О платформе'}, // 1
-    {key: false, value: 'Правила платформы'}, // 2
-    {key: false, value: 'Trust & Safety'}, // 3
-    {key: false, value: 'FAQ'}, // 4
-    {key: false, value: 'Начало'}, // 5
-    {key: false, value: 'Оформление проекта'}, // 6
-    {key: false, value: 'Вознаграждения'}, // 7
-    {key: false, value: 'Бюджет проекта'}, // 8
-    {key: false, value: 'Стратегия продвижения'}, // 9
-    {key: false, value: 'Сбор средств'}, // 10
-    {key: false, value: 'Заключение'}, // 11
+    { key: true, value: 'Как работает платформа' }, // 0
+    { key: false, value: 'О платформе' }, // 1
+    { key: false, value: 'Правила платформы' }, // 2
+    { key: false, value: 'Trust & Safety' }, // 3
+    { key: false, value: 'FAQ' }, // 4
+    { key: false, value: 'Начало' }, // 5
+    { key: false, value: 'Оформление проекта' }, // 6
+    { key: false, value: 'Вознаграждения' }, // 7
+    { key: false, value: 'Бюджет проекта' }, // 8
+    { key: false, value: 'Стратегия продвижения' }, // 9
+    { key: false, value: 'Сбор средств' }, // 10
+    { key: false, value: 'Заключение' }, // 11
+  ];
+
+  public faq: GenericLookupItem<string, string>[] = [
+    {
+      key: 'Что такое ' + this.platformName + ' ?',
+      value: this.platformName + 'это краудфандинговая площадка, позволяющая авторам находить средства на реализацию своих идей. Зарегистрировавшись на площадке, любой желающий может создать проект и собрать средства на организацию фестиваля, запись музыкального альбома, съёмку фильма, издание книги, открытие художественной студии, разработку компьютерного приложения, выпуск нового продукта, строительство детской площадки, проведение благотворительной акции - словом, на любой коммерческий, образовательный, социальный или благотворительный проект, который может принести пользу и заинтересовать других.'
+    },
+    {
+      key: 'Что такое краудфандинг ?',
+      value: 'Краудфа́ндинг (от английских слов сrowd — «толпа», и funding — «финансирование») — это коллективное сотрудничество людей (спонсоров), которые добровольно объединяют свои деньги или другие ресурсы вместе, как правило через Интернет, чтобы поддержать идеи других людей или организаций (авторов). При этом сбор средств осуществляется для самых различных целей.'
+    },
   ];
 
   ngOnInit() {
