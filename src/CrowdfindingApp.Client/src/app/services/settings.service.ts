a80c107580de8ff18081040091736fcb88cbb8bf
PeterKvayt <peter.kvayt@gmail.com>
2021-06-19 16:04:13 +0300

    Client settings update.

--- src/CrowdfindingApp.Client/src/app/services/settings.service.ts
+++ src/CrowdfindingApp.Client/src/app/services/settings.service.ts
@@ -1,7 +1,7 @@
 export class Settings{
 
-  //public static host = 'https://localhost/'; // local
-  public static host = 'https://localhost:44373/'; // vs
+  public static host = 'https://localhost/api/'; // local
+  //public static host = 'https://localhost:44373/'; // vs
   // public static host = 'http://localhost:5000/';
   public static apiRoute = Settings.host;
   public static accessToken = 'access_token';
